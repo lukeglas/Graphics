8c7faefde8ca1adff2c76a277dc91e4461beb48b
lukeglas <luke@nielsglas.nl>
2021-12-08 19:23:13 +0100

    startup scene

--- main.js
+++ main.js
@@ -1,125 +1,143 @@
 import * as THREE from 'https://unpkg.com/three@0.126.1/build/three.module.js';
 import { GUI } from 'dat.gui'
-//import { OrbitControls} from '.orbit-controls-es6';
 
-//create renderer
-const renderer = new THREE.WebGLRenderer(
-	{
-	antialias: true
-})
-renderer.setSize(innerWidth, innerHeight)
-renderer.setPixelRatio(window.
-	devicePixelRatio) 
-document.body.appendChild(renderer.
-	domElement)
+var scene, camera, renderer, sphere;
+var floor, ambientLight, light;
 
-// create scene
-const scene = new THREE.Scene()
+var keyboard = {};
+var person = { height:1.8, speed:0.2, turnSpeed:Math.PI*0.02 };
 
-//create sphere
+function init(){
+	//create scene
+	scene = new THREE.Scene();
+	
+	//create Camera
+	const fov = 90
+	const aspect = innerWidth / innerHeight
+	const near = 0.1
+	const far = 1000
+	camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+	camera.position.set(0, person.height, -5);
+	camera.lookAt(new THREE.Vector3(0,person.height,0));
 
-const sphere = new THREE.Mesh(new THREE.
-	SphereGeometry(50, 64, 64), new THREE.
-	MeshPhysicalMaterial({map: new THREE.TextureLoader().load('./img/glass.jpg'), color: 0x8418ca
-    })
-) 
-//var x = sphere.position.x;
-//var z = sphere.position.z;
+	// create sphere
+	sphere = new THREE.Mesh(new THREE.
+		SphereGeometry(1, 64, 64),  new THREE.
+		MeshPhysicalMaterial({map: new THREE.TextureLoader().load('./img/glass.jpg'), color: 0x8418ca
+		})
+	) 
+	sphere.position.y += 1.5;
+	sphere.receiveShadow = true;
+	sphere.castShadow = true;
+	scene.add(sphere) 
 
-//sphere.position.x = x * Math.cos(theta) + z * Math.sin(theta);
-//sphere.position.z = z * Math.cos(theta) - x * Math.sin(theta);
+	//create planes
+	floor = new THREE.Mesh(
+		new THREE.PlaneGeometry(20,20, 10,10),
+		new THREE.MeshPhongMaterial({color:0xffffff, side:THREE.DoubleSide})
+	);
+	floor.rotation.x -= Math.PI / 2; 
+	floor.receiveShadow = true;
+	scene.add(floor);
 
+	const wall = new THREE.Mesh(
+		new THREE.PlaneGeometry(20,20, 5,5),
+		new THREE.MeshPhongMaterial({color:0xffffff, side:THREE.DoubleSide})
+	);
+	wall.rotation.x -= Math.PI; 
+	wall.position.z = 10
+	wall.position.y = 10
+	wall.receiveShadow = true;
+	scene.add(wall);
+	
+	const wall2 = new THREE.Mesh(
+		new THREE.PlaneGeometry(20,20, 10,10),
+		new THREE.MeshPhongMaterial({color:0xffffff, side:THREE.DoubleSide})
+	);
+	wall2.rotation.y -= Math.PI/2; 
+	wall2.position.x = 10
+	wall2.position.y = 10
+	wall2.receiveShadow = true;
+	scene.add(wall2);
+	
+	//create lights
+	ambientLight = new THREE.AmbientLight(0xffffff, 0.2);
+	scene.add(ambientLight);
+	
+	light = new THREE.PointLight(0xffffff, 0.8, 18);
+	light.position.set(-3,6,-3);
+	light.castShadow = true;
+	light.shadow.camera.near = 0.1;
+	light.shadow.camera.far = 25;
+	scene.add(light);	
+	
+	//create renderer
+	renderer = new THREE.WebGLRenderer({antialias: true});
+	renderer.setSize(innerWidth, innerHeight);
+	renderer.shadowMap.enabled = true;
+	renderer.shadowMap.type = THREE.BasicShadowMap;
+	document.body.appendChild(renderer.domElement);
 
-scene.add(sphere) 
-//create camera
-const fov = 50
-const aspect = innerWidth / innerHeight
-const near = 1
-const far = 1000
-const camera = new THREE.
-PerspectiveCamera(
-	fov,
-	aspect,
-	near,
-	far
-)
-camera.position.set(0,0,500)
-
-//controls = new OrbitControls(camera, renderer.domElement)
-
-// create plane
-const square = new THREE.BoxGeometry(1,0.1,1)
-const lightsquare = new THREE.MeshBasicMaterial({color: 0xE0C4A8})
-const darksquare = new THREE.MeshBasicMaterial({color: 0x6A4236})
-
-const board = new THREE.Group()
-
-for (let x =0; x< 10; x++){
-	for( let z = 0; z <10; z++){
-		let cube
-		if (z % 2 ==0 ){
-			cube =new THREE.Mesh(square, x % 2 ==0 ? lightsquare : darksquare);
-			} else {
-				cube =new THREE.Mesh(square, x % 2 ==0 ? darksquare : lightsquare);
-			}
-			cube.position.set(x,0,z);
-			board.add(cube);
-	}
-}
-scene.add(board)
-
-
-// create Light
-const pointlight = new THREE.PointLight(0xffffff,1)
-pointlight.position.set(200,200,200)
-scene.add(pointlight)
-var lightAmb = new THREE.AmbientLight(0x444444);
-scene.add(lightAmb);
-
-//create Gui
-const gui = new GUI()
-const resFolder = gui.addFolder('Resolution')
-//resFolder.add(aspect, 'Resolution', 0, Math.PI * 2)
-//resFolder.open()
-
-const cameraFolder = gui.addFolder('Camera')
-cameraFolder.add(camera.position, 'z', 100, 250)
-cameraFolder.open()
-
-//add table
-//var mtlLoaderTable = new THREE.MTLLoader();
-//mtlLoaderChair.setBaseUrl('assets/table/');
-///mtlLoaderChair.setPath('assets/table/');
-//mtlLoaderChair.load('table.mtl', function (materials) {
-//materials.preload();
-//materials.materials.fusta_taula.map.magFilter = THREE.NearestFilter;
-//materials.materials.fusta_taula.map.minFilter = THREE.LinearFilter;
-
-//var objLoaderChair = new THREE.OBJLoader();
-//objLoaderChair.setMaterials(materials);
-//objLoaderChair.setPath('assets/table/');
-//objLoaderChair.load('table.obj', function (object) {
-//object.position.y = -40;
-//object.position.z = 330;
-//object.scale.set(80,80,80);
-//object.rotation.x = .01;
-//scene.add(object);
-//});
-//});
+	//create Gui
+	const gui = new GUI()
+	const resFolder = gui.addFolder('Resolution')
+	resFolder.add(camera, 'aspect', (innerWidth / 2) + (innerWidth /2) , innerWidth  / innerWidth )
+	resFolder.open()
 
 
-
-
-if (typeof browser === "undefined") {
-    var browser = chrome;
+	animate();
 }
 
 function animate(){
-	//controls.update()
-	renderer.render( scene, camera );
-	requestAnimationFrame(animate)
-	sphere.rotation.x -= 0.01
+	requestAnimationFrame(animate);
 	
+	sphere.rotation.x += 0.01;
+
+	// Keyboard movement inputs
+	if(keyboard[87]){ // W key
+		camera.position.x -= Math.sin(camera.rotation.y) * person.speed;
+		camera.position.z -= -Math.cos(camera.rotation.y) * person.speed;
+	}
+	if(keyboard[83]){ // S key
+		camera.position.x += Math.sin(camera.rotation.y) * person.speed;
+		camera.position.z += -Math.cos(camera.rotation.y) * person.speed;
+	}
+	if(keyboard[65]){ // A key
+		// Redirect motion by 90 degrees
+		camera.position.x += Math.sin(camera.rotation.y + Math.PI/2) * person.speed;
+		camera.position.z += -Math.cos(camera.rotation.y + Math.PI/2) * person.speed;
+	}
+	if(keyboard[68]){ // D key
+		camera.position.x += Math.sin(camera.rotation.y - Math.PI/2) * person.speed;
+		camera.position.z += -Math.cos(camera.rotation.y - Math.PI/2) * person.speed;
+	}
 	
+	// Keyboard turn inputs
+	if(keyboard[37]){ // left arrow key
+		camera.rotation.y -= person.turnSpeed;
+	}
+	if(keyboard[39]){ // right arrow key
+		camera.rotation.y += person.turnSpeed;
+	}
+	if(keyboard[38]){ // up arrow key
+		camera.position.y += person.turnSpeed;
+	}
+	if(keyboard[40]){ // down arrow key
+		camera.position.y -= person.turnSpeed;
+	}
+	
+	renderer.render(scene, camera);
 }
-animate()
+
+function keyDown(event){
+	keyboard[event.keyCode] = true;
+}
+
+function keyUp(event){
+	keyboard[event.keyCode] = false;
+}
+
+window.addEventListener('keydown', keyDown);
+window.addEventListener('keyup', keyUp);
+
+window.onload = init;
